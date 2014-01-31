538417fa71b521fb24f2e6af03480355c53df124
Daryl Lau <dlau@ya.ru>
2014-01-31 12:03:29 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var co_body = require('co-body');
+
+
+module.exports = function(opts){
+  return function *(next){
+    if(this.is('application/json')){
+      this.request.req.body = yield co_body.json(this);
+    }
+    else if(this.is('application/x-www-form-urlencoded')){
+      this.request.req.body = yield co_body.form(this);
+    }
+    yield next;
+  };
+};
